15563dea7f810a47bf36dee5699e0fc9e226b0de
Gary Benson <gary@gbenson.net>
2023-02-07 10:26:10 +0000

    Switch to @app.route

--- app.py
+++ app.py
@@ -16,14 +16,23 @@ footer_text = '</body>\n</html>'
 
 app = Flask(__name__)
 
-# add a rule for the index page.
-app.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@app.route("/")
+def index():
+    return "".join((
+        header_text,
+        say_hello(),
+        instructions,
+        footer_text,
+    ))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-app.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@app.route("/<username>")
+def hello(username):
+    return "".join((
+        header_text,
+        say_hello(username),
+        home_link,
+        footer_text,
+    ))
 
 # run the app.
 if __name__ == "__main__":
